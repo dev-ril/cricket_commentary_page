29c4451a0bd6f9e6440f27a493cb810485b58c92
Ril <rilwanhuzain@gmail.com>
2021-05-18 13:44:02 +0530

    added variables

--- app/controllers/home-page.js
+++ app/controllers/home-page.js
@@ -5,9 +5,14 @@ export default Controller.extend({
   battingTeam: "Chennai Super Kings",
   runsNeeded: 100,
   overs: 15,
+  score:0,
+  noOfWickets:0,
+  over:0,
   batsmen: ['Ruturaj Gaikwad', 'Faf du Plessis', 'Moeen Ali', 'Suresh Raina', 'Ambati Rayudu', 'Ravindra Jadeja', 'MS Dhoni', 'Sam Curran', 'Dwayne Bravo', 'Shardul Thakur', 'Deepak Chahar'],
   bowler: ['Ravindra Jadeja', 'Sam Curran', 'Dwayne Bravo', ' Shardul Thakur', ' Deepak Chahar'],
-  
+  runsTaken: [0,0,0,0,0,0,0,0,0,0,0],
+  runsGiven: [0,0,0,0,0],
+  wicketsTaken: [0,0,0,0,0],
   actions: {
     toggleBody() {
       this.toggleProperty('isExpanded');
