1a5364df0776c5b52de094ec55d51e7d4876ebf4
Ril <rilwanhuzain@gmail.com>
2021-05-19 18:00:21 +0530

    added some components

--- app/components/header.js
+++ app/components/header.js
@@ -6,6 +6,6 @@ export default class HeaderComponent extends Component {
     battingTeam= "Chennai Super Kings";//batting team name
     runsNeeded= 100;//total runs needed to win the match
     overs= 15;//total no of overs
-        
+    
 }
  
--- app/components/home-page.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@action';
-
-export default class HomePageComponent extends Component {
- 
-}
-
